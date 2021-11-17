9514e5719a1df18f99482e1720074c160c9bf8b5
Victor <vikr1@yandex.ru>
2021-11-17 15:45:14 +0300

    коннекты на кнопки из Активные архивы работает

--- mainwindow.py
+++ mainwindow.py
@@ -1,11 +1,15 @@
 
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 import matplotlib
 matplotlib.use('Qt5Agg')
 from PyQt5.QtWidgets import QLabel, QPushButton, QScrollArea, QSizePolicy, QVBoxLayout, QWidget
+import pandas as pd
+import os
 
 import interface
 import filebrowser
+import globals
 
 # Класс главного окна со своим конструктором
 class MainWindow(QtWidgets.QMainWindow, interface.Ui_MainWindow):
@@ -104,12 +108,50 @@ class SubWidget(QWidget):
         self.button.setStyleSheet("background-color: rgb(138, 191, 255)")
         self.button.setText(e.mimeData().text())
         self.mainwind.lay_1.addWidget(self.button)
-
-        # коннект на нажатие кнопки Обновить
+        # коннект на клик выбранного архива в разделе Активные архивы
         self.button.clicked.connect(self.on_button)
 
     def on_button(self):
-        print('sdfgfxdsfg')
+        # извлекаем id объекта (в данном случае это у нас название архива),
+        # что является ключом для поиска в словаре
+        sender = self.sender()  # получаем id экземпляра
+        key = sender.text()     # получаем из экземпляра текст
+        # помещаем название архива в промежут. переменную
+        globals.archname_archpath[0] = key
+        # извлекаем из словаря соответствующий имени путь и помещаем в промежут. переменную
+        globals.archname_archpath[1] = globals.arch_dict[key]
+        print(globals.archname_archpath)
+
+        dir_path = globals.archname_archpath[1]
+        objects_list = os.listdir(dir_path)           # получаем список имён всех объектов папки
+        print(objects_list)
+
+        # ищем первый попавшийся файл csv и получаем из него список параметров
+        for i in range(len(objects_list)):
+            if objects_list[i].endswith('csv'):
+                obj_path = os.path.join(dir_path, objects_list[i])   # соединяем путь к папке объекта с именем объекта
+                dataframe = pd.read_csv(obj_path)                    # читаем данные из csv в dataframe
+                names = dataframe.columns.tolist()                   # получаем список имён колонок (список параметров)
+                break
+            else:
+                print('false')
+        print(names)
+
+        # загружаем список параметров в раздел Все параметры архива
+        # for i in range(len(names)):
+
+        #     self.button1 = QPushButton()
+        #     self.button1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
+        #     self.button1.setMinimumHeight(20)
+        #     self.button1.setAlignment(Qt.AlignHLeft | Qt.AlignVCenter)
+        #     self.button1.setStyleSheet("background-color: rgb(138, 191, 255)")
+        #     self.button1.setText(names[i])
+        #     self.mainwind.verticalLayout_10.addWidget(self.button1)
+        #     # коннект на клик выбранного архива в разделе Активные архивы
+        #     # self.button.clicked.connect(self.on_button1)
+
+
+
 
 
         
