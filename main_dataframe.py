a31d2b8d615a17a9eb1d01d2a71d575a4a9534b1
Victor <vikr1@yandex.ru>
2021-11-22 13:56:21 +0300

    получил вспом. датафрейм sub_df для каждого параметра из раздела Активные параметры

--- main_dataframe.py
+++ main_dataframe.py
@@ -6,79 +6,70 @@ import globals
 class MainDataframe():
     def __init__(self, mainwindow) -> None:
         self.mainwind = mainwindow
-        param_name = ''
-        csv_list = []   # пустой список для имён csv-шников
-        # пустая вспомогательная df (будет содержать сконкатинированные данные из всех csv-шников по взятому параметру)
-        sub_df = pd.DataFrame({})
 
         # определяем кол-во объектов в контейнере с параметрами
         layout = self.mainwind.lay_b_1
-        objs_number = layout.count()
+        objs_number = layout.count()    # кол-во объектов (параметров)  
+
+        # итерируемся по элементам контейнера (параметрам)
         if objs_number:
             for i in range(objs_number):
-                item = layout.itemAt(i)                     # получаем i-тый элемент контейнера
-                label = item.widget()                       # получаем лейбел
-                text = label.text()                         # извлекаем название лейбла
-                a = text.split(' *')                        # получаем список слов названия
-                param_name = a[1]                           # имя параметра
-                a[0] = a[0].replace("[", "")                # удаляем скобки из имени архива
-                a[0] = a[0].replace("]", "")
-                arch_name = a[0]                            # получаем имя архива
-                arch_path = globals.arch_dict[arch_name]    # путь к архиву
-                objects_list = os.listdir(arch_path)        # список всех объектов в папке
-                # составляем список только из csv-шников
-                csv_list.clear()    # очищаем список для каждой итерации по элементам контейнера
-                for i in range(len(objects_list)):
-                    if objects_list[i].endswith('csv'):
-                        csv_list.append(objects_list[i]) # здесь список csv-шников
-
-                # итерируемся по списку файлов в текущем архиве:
-                for i in range(len(csv_list)):
-                    # соединяем путь к архиву с именем файла
-                    full_path = os.path.join(arch_path, csv_list[i])
-
-                    # получаем из csv-файла временный dataframe (tmp_df)
-                    tmp_df = pd.read_csv(full_path)      # читаем все данные из текущего csv-шника в dataframe
-
-                    # извлекаем из tmp_df только нужные колонки (времени и данные параметра)
-                    nm = tmp_df.columns                     # список названий колонок
-                    tmp_df_2 = tmp_df[[nm[1], param_name]]  # извлекли нужные колонки
-                    nm2 = tmp_df_2.columns                  # список названий колонок (для переименования первой)
-                    tmp_df_2 = tmp_df_2.rename(columns={nm2[0] : 'date_time'}) # переименовываем 0-ю колонку
-
-                    # конкатинируем с sub_df
-                    sub_df = pd.concat([sub_df, tmp_df_2])
-
-                    # сбрасываем индексацию
-                    sub_df.reset_index(drop=True, inplace=True)
-
-
+                # для каждого параметра из контейнера формируем датафрейм
+                sub_df = self.df_formation(i, layout)
                 print(sub_df)
-                # print(sub_df.head(5))
-                
-                
-
-                
         else:
             print('no objects')
 
 
     
+    # @brief  Метод, формирующий вспомогательную датафрейм.
+    # @detail Метод берёт i-тый параметр из контейнера, определяет архив, к которому принадлежит параметр,
+    # выдёргивает из каждого csv-шника лименно этот параметр и конкатинирует их все во вспомогательную датафрейм sub_df.
+    # @param  i - счётчик для итерации по элементам контейнера с параметрами;
+    # @param  layout - контейнер с параметрами;
+    # @retval sub_df - датафрейм, состоящая из сконкатинированных столбцов конкретно по заданному параметру.
+    def df_formation(self, i, layout):
+            param_name = ''
+            csv_list = []   # пустой список для имён csv-шников
+            # пустая вспомогательная df (будет содержать сконкатинированные данные из всех csv-шников по взятому параметру)
+            df = pd.DataFrame({})
 
+            item = layout.itemAt(i)                     # получаем i-тый элемент контейнера
+            label = item.widget()                       # получаем лейбел
+            text = label.text()                         # извлекаем название лейбла
+            a = text.split(' *')                        # получаем список слов названия
+            param_name = a[1]                           # имя параметра
+            a[0] = a[0].replace("[", "")                # удаляем скобки из имени архива
+            a[0] = a[0].replace("]", "")
+            arch_name = a[0]                            # получаем имя архива
+            arch_path = globals.arch_dict[arch_name]    # путь к архиву
+            objects_list = os.listdir(arch_path)        # список всех объектов в папке
+            # составляем список только из csv-шников
+            csv_list.clear()    # очищаем список для каждой итерации по элементам контейнера
+            for i in range(len(objects_list)):
+                if objects_list[i].endswith('csv'):
+                    csv_list.append(objects_list[i]) # здесь список csv-шников
 
+            # итерируемся по списку файлов в текущем архиве:
+            df = pd.DataFrame({})   # обнуляем df перед каждым архивом                
+            for i in range(len(csv_list)):
+                # соединяем путь к архиву с именем файла
+                full_path = os.path.join(arch_path, csv_list[i])
 
+                # получаем из csv-файла временный dataframe (tmp_df)
+                tmp_df = pd.read_csv(full_path)      # читаем все данные из текущего csv-шника в dataframe
 
+                # извлекаем из tmp_df только нужные колонки (времени и данные параметра)
+                nm = tmp_df.columns                     # список названий колонок
+                tmp_df_2 = tmp_df[[nm[1], param_name]]  # извлекли нужные колонки
+                nm2 = tmp_df_2.columns                  # список названий колонок (для переименования первой)
+                tmp_df_2 = tmp_df_2.rename(columns={nm2[0] : 'date_time'}) # переименовываем 0-ю колонку
 
+                # конкатинируем с df
+                df = pd.concat([df, tmp_df_2])
 
+            # сбрасываем индексацию
+            df.reset_index(drop=True, inplace=True)
+            return df
 
-
-    # метод, формирующий вспомогательую датафрейм sub_df
-    # def sub_df_formation(self, layout):
-        # while layout.count():
-        #     item = layout.takeAt(0)
-        #     widget = item.widget()
-        #     if widget is not None:
-        #         widget.deleteLater()
-        #     else:
-        #         self.clearLayout(item.layout())
         
